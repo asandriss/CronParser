c0319222f2144361d8c09468c494fb70b4498f3a
Fedor Hajdu <fedorhajdu@gmail.com>
2025-05-27 17:01:47 +0100

    improve help text

--- CronParser/Program.cs
+++ CronParser/Program.cs
@@ -4,7 +4,21 @@ try
 {
     if (args.Length != 1)
     {
-        Console.WriteLine("Usage: CronParser \"<cron expression>\"");
+        var helpText = """
+                     Usage: CronParser <cron expression>
+
+                     Standard cron expression format:
+                       minute       0-59
+                       hour         0-23
+                       day of month 1-31
+                       month        1-12
+                       day of week  0-6 (0 = Sunday)
+                       
+                     Example: CronParser "*/15 0 1,15 * 1-5 /usr/bin/find"
+                       This will schedule the command to run every 15 minutes at hour 0 on the 1st and 15th of each month, Monday to Friday.
+                     """;
+        
+        Console.WriteLine(helpText);
         return;
     }
 
