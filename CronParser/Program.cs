52f3e40d9e961839ae126c2b80d7d8bab981607b
Fedor Hajdu <fedorhajdu@gmail.com>
2025-05-27 14:27:21 +0100

    Update program to run

--- CronParser/Program.cs
+++ CronParser/Program.cs
@@ -1,3 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello, World!");
+using CronParser;
+
+try
+{
+    if (args.Length != 1)
+    {
+        Console.WriteLine("Usage: CronParser \"<cron expression>\"");
+        return;
+    }
+
+    var schedule = CronScheduleFactory.Parse(args[0]);
+    CronSchedulePrinter.Print(schedule, Console.Out);
+}
+catch (Exception ex)
+{
+    Console.Error.WriteLine($"Error: {ex.Message}");
+    Environment.Exit(1);
+}
