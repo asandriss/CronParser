8a2ac7beb99c4dc3015dc5c92217850d049c53a0
Fedor Hajdu <fedorhajdu@gmail.com>
2025-06-02 12:11:13 +0100

    Year with a star

--- CronParser/CronScheduleFactory.cs
+++ CronParser/CronScheduleFactory.cs
@@ -4,21 +4,18 @@ public static class CronScheduleFactory
 {
    public static CronSchedule Parse(string expression)
    {
-      List<int> year = [];
-      string command = string.Empty;
-      
       if (string.IsNullOrWhiteSpace(expression))
          throw new ArgumentException("Cron expression cannot be empty");
       
-      var parts = expression.Split(' ', 6, StringSplitOptions.RemoveEmptyEntries);
+      var parts = expression.Split(' ', 7, StringSplitOptions.RemoveEmptyEntries);
 
-      if (parts.Length != 6)
+      if (parts.Length < 6)
          throw new ArgumentException("Cron expression must contain 5 time fields and a command");
 
-      if (!parts[5].StartsWith('.') && !parts[5].StartsWith('/'))
+      if (parts[5].StartsWith('.') || parts[5].StartsWith('/'))
       {
-         year = FieldParser.Parse(parts[5].Split(' ').First(), 2000, 3000);
-         command = string.Join(' ', parts[5].Split(' ').Skip(1).ToArray());
+         var newExpression = $"{parts[0]} {parts[1]} {parts[2]} {parts[3]} {parts[4]} * {parts[5]}";
+         return Parse(newExpression);
       }
 
       return new CronSchedule(
@@ -27,8 +24,8 @@ public static class CronScheduleFactory
          daysOfMonth: FieldParser.Parse(parts[2], 1, 31),
          months: FieldParser.Parse(parts[3], 1, 12),
          daysOfWeek: FieldParser.Parse(parts[4], 0, 6),
-         year: year,
-         command: command == String.Empty ? parts[5] : command
+         year: FieldParser.Parse(parts[5], 2000, 3000),
+         command: parts[6]
       );
    }
 }
